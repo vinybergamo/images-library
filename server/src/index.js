7e5be6c437bf25b0dfb466dc589a35226fba5173
vinybergamo <vinybergamo@gmail.com>
2022-09-07 11:22:23 -0300

    Fix: Local URL

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
@@ -13,7 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 app.use(require("./routes/index"));
 
 mongoose
